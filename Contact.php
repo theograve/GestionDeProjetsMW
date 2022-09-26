ead9dbaf47c0186f176b0df4d52bd64a26aa5c05
Skiyps <79086232+theograve@users.noreply.github.com>
2022-09-26 16:18:42 +0200

    Add files via upload

--- Contact.php
+++ Contact.php
@@ -1,1 +1,63 @@
+<?php
 
+require_once 'Customer.php';
+require_once 'Host.php';
+
+class Contact
+{
+	private string $email;
+	private int $phoneNumber;
+    private string $role;
+    private Customer $customer;
+    private Host $host;
+
+	public function getEmail(): string
+	{
+		return $this->email;
+	}
+
+	public function setEmail(string $email): void
+	{
+		$this->email = $email;
+	}
+
+	public function getPhoneNumber(): int
+	{
+		return $this->phoneNumber;
+	}
+
+    public function setPhoneNumber(int $phoneNumber): void
+	{
+		$this->phoneNumber = $phoneNumber;
+	}
+
+    public function getRole(): string
+	{
+		return $this->role;
+	}
+
+    public function setRole(string $role): void
+	{
+		$this->role = $role;
+	}
+
+    public function getCustomer(): Customer
+	{
+		return $this->customer;
+	}
+
+    public function setCustomer(Customer $customer): void
+	{
+		$this->customer = $customer;
+	}
+
+    public function getHost(): Host
+	{
+		return $this->host;
+	}
+
+    public function setHost(Host $host): void
+	{
+		$this->host = $host;
+	}
+}
--- Customer.php
+++ Customer.php
@@ -1,1 +1,37 @@
+<?php
+    Class Customer
+    {
+        private int $codeCustomer;
+        private string $nameCustomer;
+        private string $notesCustomer;
 
+        public function getCodeCustomer(): int
+        {
+            return $this->codeCustomer;
+        }
+
+        public function setCodeCustomer(int $codeCustomer): void
+        {
+            $this->codeCustomer = $codeCustomer;
+        }
+
+        public function getNameCustomer(): string
+	    {
+		    return $this->nameCustomer;
+	    }
+
+        public function setNameCustomer(string $nameCustomer): void
+        {
+            $this->nameCustomer = $nameCustomer;
+        }
+
+        public function getNotesCustomer(): string
+	    {
+		    return $this->notesCustomer;
+	    }
+
+        public function setNotesCustomer(string $notesCustomer): void
+        {
+            $this->notesCustomer = $notesCustomer;
+        }
+    }
