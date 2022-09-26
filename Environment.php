1c00b536b8e88fd3bca4e8c67781720dbefaebae
SGNoxx <79085868+SGNoxx@users.noreply.github.com>
2022-09-26 17:05:32 +0200

    Adding Environment, Project / MEA

--- Environment.php
+++ Environment.php
@@ -1,1 +1,95 @@
+<?php
 
+require_once 'Project.php';
+
+class Environment
+{
+    private string $name;
+    private string $link;
+    private string $ipAddress;
+    private string $sshPort;
+    private string $sshName;
+    private string $phpmyadminLink;
+    private string $ipRestriction;
+    private Project $project;
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getLink(): string
+    {
+        return $this->link;
+    }
+
+    public function setLink(string $link): void
+    {
+        $this->link = $link;
+    }
+
+    public function getIpAddress(): string
+    {
+        return $this->ipAddress;
+    }
+
+    public function setIpAddress(string $ipAddress): void
+    {
+        $this->ipAddress = $ipAddress;
+    }
+
+    public function getSshPort(): string
+    {
+        return $this->sshPort;
+    }
+
+    public function setSshPort(string $sshPort): void
+    {
+        $this->sshPort = $sshPort;
+    }
+
+    public function getSshName(): string
+    {
+        return $this->sshPName;
+    }
+
+    public function setSshName(string $sshName): void
+    {
+        $this->sshName = $sshName;
+    }
+
+    public function getPhpmyadminLink(): string
+    {
+        return $this->phpmyadminLink;
+    }
+
+    public function setPhpmyadminLink(string $phpmyadminLink): void
+    {
+        $this->phpmyadminLink = $phpmyadminLink;
+    }
+
+    public function getIpRestriction(): string
+    {
+        return $this->phpmyadminLink;
+    }
+
+    public function setIpRestriction(string $ipRestriction): void
+    {
+        $this->ipRestriction = $ipRestriction;
+    }
+
+    public function getProject(): Project
+    {
+        return $this->project;
+    }
+
+    public function setProject(Project $project): void
+    {
+        $this->project = $project;
+    }
+}
--- Project.php
+++ Project.php
@@ -1,1 +1,96 @@
+<?php
 
+require_once 'Host.php';
+require_once 'Customer.php';
+
+class Project
+{
+    private int $code;
+    private string $name;
+    private string $lastPass;
+    private string $linkMockUps;
+    private string $managedServer;
+    private string $notes;
+    private Customer $customer;
+    private Host $host;
+
+    public function getCode(): int
+    {
+        return $this->code;
+    }
+
+    public function setCode(int $code): void
+    {
+        $this->code = $code;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getLastPass(): string
+    {
+        return $this->lastPass;
+    }
+
+    public function setLastPass(string $lastPass): void
+    {
+        $this->lastPass = $lastPass;
+    }
+
+    public function getLinkMockUps(): string
+    {
+        return $this->linkMockUps;
+    }
+
+    public function setLinkMockUps(string $linkMockUps): void
+    {
+        $this->linkMockUps = $linkMockUps;
+    }
+
+    public function getManagedServer(): string
+    {
+        return $this->managedServer;
+    }
+
+    public function setManagedServer(string $managedServer): void
+    {
+        $this->managedServer = $managedServer;
+    }
+
+    public function getNotes(): string
+    {
+        return $this->notes;
+    }
+
+    public function setNotes(string $notes): void
+    {
+        $this->notes = $notes;
+    }
+
+    public function getCustomer(): Customer
+    {
+        return $this->customer;
+    }
+
+    public function setCustomer(Customer $customer): void
+    {
+        $this->customer = $customer;
+    }
+
+    public function getHost(): Host
+    {
+        return $this->host;
+    }
+
+    public function setHost(Host $host): void
+    {
+        $this->host = $host;
+    }
+}
