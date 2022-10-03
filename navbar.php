5e680f3b138d6dadf1a846b412bc1bae73c7a12a
SGNoxx <79085868+SGNoxx@users.noreply.github.com>
2022-10-03 16:36:31 +0200

    navbar + sidebar + footer + logo
    Ajout de la navbar constituée de la sidebar et du logo ainsi qu'un fichier contenant le footer

---
+++ navbar.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<head>
+    <meta charset="utf-8">
+    <title>Gestion de projets</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<nav>
+    <div class="navbar">
+        <div class="container nav-container">
+            <input class="checkbox" type="checkbox" name="" id="" />
+            <div class="hamburger-lines">
+                <span class="line line1"></span>
+                <span class="line line2"></span>
+                <span class="line line3"></span>
+            </div>
+            <div class="logo">
+                <img src="logo-mentalworks-blanc.png" style="width:184px">
+            </div>
+            <div class="menu-items">
+                <li><a href="#">Tableau de bord</a></li>
+                <li><a href="#">Projets</a></li>
+                <li><a href="#">Clients</a></li>
+                <li><a href="#">Hébergeurs</a></li>
+            </div>
+        </div>
+    </div>
+</nav>
+<?php
+include ('footer.php');
+?>
+</body>
+</html>
