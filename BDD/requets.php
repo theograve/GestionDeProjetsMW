3f7f0292c5bdf1d94161a73c085c554720e0807d
Skiyps <79086232+theograve@users.noreply.github.com>
2022-11-06 23:42:46 +0100

    request update projet

--- BDD/requets.php
+++ BDD/requets.php
@@ -65,6 +65,38 @@ if(isset($_POST['modifyHost'])){
     }
 }
 
+if(isset($_POST['insertProjet'])){
+    // récupérer les valeurs
+    $name = $_POST['name'];
+    $notes = $_POST['notes'];
+    $code = $_POST['id'];
+
+    // Requête mysql pour insérer des données
+    $sql = "INSERT INTO projet(name, notes, code, lastpass_folder ,link_mock_ups ,managed_server ,host_id, customer_id) VALUES (:name, :notes, '0000', :lastpass_folder, :link_mock_ups, :managed_server, :host_id, :customer_id)";
+    $res = $db->prepare($sql);
+    $exec = $res->execute(array(":name"=>$name,":notes"=>$notes));
+
+    // vérifier si la requête d'insertion a réussi
+    if($exec){
+        header('Location: ../ajoutProjet.php');
+    }else{
+        echo "Échec de l'opération d'insertion";
+    }
+}
+
+if(isset($_POST['modifyProjet'])){
+    $sql = "UPDATE projet SET name = :name, notes = :notes, code = :code, lastpass_folder = :lastpass_folder ,link_mock_ups = :link_mock_ups ,managed_server = :managed_server ,host_id = :host_id, customer_id = :customer_id WHERE id = :id";
+    $statement = $db->prepare($sql);
+    foreach ($_POST['id'] as $id) {
+        $name = $_POST['name'][$id] ?? '';
+        $notes = $_POST['notes'][$id] ?? '';
+        $code = $_POST['code'][$id] ?? '';
+        $statement->execute(compact('id', 'name', 'notes', 'code'));
+
+        header('Location: ../modifProjet.php');
+    }
+}
+
 $sqlClient = "SELECT * FROM customer";
 $statement = $db->prepare($sqlClient);
 $statement->execute();
@@ -74,3 +106,8 @@ $sqlHost = "SELECT * FROM host";
 $statement = $db->prepare($sqlHost);
 $statement->execute();
 $listHost = $statement->fetchAll(PDO::FETCH_OBJ);
+
+$sqlProjet = "SELECT * FROM projet";
+$statement = $db->prepare($sqlProjet);
+$statement->execute();
+$listProjet = $statement->fetchAll(PDO::FETCH_OBJ);
