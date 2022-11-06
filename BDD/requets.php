175f7195c982783f552a3ce03cbaa005d9ac72ed
SGNoxx <79085868+SGNoxx@users.noreply.github.com>
2022-11-06 23:17:02 +0100

    modification des requets
    modification des requets pour ajouter l'host et modifier le client

--- BDD/requets.php
+++ BDD/requets.php
@@ -1,7 +1,7 @@
 <?php
 require('connexion.php');
 
-if(isset($_POST['insert'])){
+if(isset($_POST['insertCustomer'])){
     // récupérer les valeurs
     $name = $_POST['name'];
     $notes = $_POST['notes'];
@@ -14,13 +14,13 @@ if(isset($_POST['insert'])){
 
     // vérifier si la requête d'insertion a réussi
     if($exec){
-        header('Location: ../ajout.php');
+        header('Location: ../ajoutClient.php');
     }else{
         echo "Échec de l'opération d'insertion";
     }
 }
 
-if(isset($_POST['modify'])){
+if(isset($_POST['modifyCustomer'])){
     $sql = "UPDATE customer SET name = :name, notes = :notes, code = :code WHERE id = :id";
     $statement = $db->prepare($sql);
     foreach ($_POST['id'] as $id) {
@@ -29,11 +29,48 @@ if(isset($_POST['modify'])){
         $code = $_POST['code'][$id] ?? '';
         $statement->execute(compact('id', 'name', 'notes', 'code'));
 
-        header('Location: ../modif.php');
+        header('Location: ../modifClient.php');
     }
 }
 
-$sql = "SELECT * FROM customer";
-$statement = $db->prepare($sql);
+if(isset($_POST['insertHost'])){
+    // récupérer les valeurs
+    $name = $_POST['name'];
+    $notes = $_POST['notes'];
+    $code = $_POST['id'];
+
+    // Requête mysql pour insérer des données
+    $sql = "INSERT INTO host(name, notes, code) VALUES (:name, :notes, '0000')";
+    $res = $db->prepare($sql);
+    $exec = $res->execute(array(":name"=>$name,":notes"=>$notes));
+
+    // vérifier si la requête d'insertion a réussi
+    if($exec){
+        header('Location: ../ajoutClient.php');
+    }else{
+        echo "Échec de l'opération d'insertion";
+    }
+}
+
+if(isset($_POST['modifyHost'])){
+    $sql = "UPDATE host SET name = :name, notes = :notes, code = :code WHERE id = :id";
+    $statement = $db->prepare($sql);
+    foreach ($_POST['id'] as $id) {
+        $name = $_POST['name'][$id] ?? '';
+        $notes = $_POST['notes'][$id] ?? '';
+        $code = $_POST['code'][$id] ?? '';
+        $statement->execute(compact('id', 'name', 'notes', 'code'));
+
+        header('Location: ../modifHebergeur.php');
+    }
+}
+
+$sqlClient = "SELECT * FROM customer";
+$statement = $db->prepare($sqlClient);
 $statement->execute();
 $listClient = $statement->fetchAll(PDO::FETCH_OBJ);
+
+$sqlHost = "SELECT * FROM host";
+$statement = $db->prepare($sqlHost);
+$statement->execute();
+$listHost = $statement->fetchAll(PDO::FETCH_OBJ);
